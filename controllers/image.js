70ec0ba607d82dada421b8f2d2e41789ba67fd43
Kumovic Nenad <nenad.kumovic@it4communication.ch>
2019-12-12 16:16:55 +0100

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,33 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+	apiKey: 'acc70f238d554b009b8da73619ce52fe'
+ });
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(
+			Clarifai.FACE_DETECT_MODEL,
+			req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('Unable to work with api'))
+};
+
+const handleImage =  (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0])
+		})
+		.catch(err => res.status(400).json('Unable to get entries'))
+};
+
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+}
